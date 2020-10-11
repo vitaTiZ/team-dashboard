dd988a105d3fe5e15e871439bbc185493472ecc8
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-10-11 15:22:48 +0200

    Add firebase plugin + config dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require('dotenv').config()
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.apiKey,
+          authDomain: process.env.authDomain,
+          databaseURL: process.env.databaseURL,
+          projectId: process.env.projectId,
+          storageBucket: process.env.storageBucket,
+          messagingSenderId: process.env.messagingSenderId,
+          appId: process.env.appId
+        }
+      }
+    }
+  ],
 }
