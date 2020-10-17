a85715fbe8e89bedbf870d6266cd55bddaa8692f
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-10-17 09:25:08 +0200

    Create new component test

---
+++ src/components/test.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Test = ({name}) => {
+    return <div>{name}</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import {Test} from "../components/test"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Test name="test"/>
+      <Test name="test1"/>
+    </div>
+  )
 }
