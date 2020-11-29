3cbaef2b846ba2ee8d731fe604a18b07c38fead7
Marco Terzolo <marco.terzolo.8080@gmail.com>
2020-11-29 14:35:50 +0100

    added first components - header

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
           firestore: true
         }
       }
-    }
+    },
+    `gatsby-plugin-sass`
   ],
 }
--- src/components/header/header-title/header-title.jsx
+++ src/components/header/header-title/header-title.jsx
@@ -1,5 +1,14 @@
 import React from "react"
+import './header-title.scss'
 
 export const HeaderTitle = () => {
-    return <div>HEADER</div>
+  return (
+    <div id="logo">
+      <span className="image avatar">
+        <img src="https://via.placeholder.com/150" alt="" />
+      </span>
+      <h1 id="title">Jane Doe</h1>
+      <p>Hyperspace Engineer</p>
+    </div>
+  )
 }
